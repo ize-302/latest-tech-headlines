75b518cbbfd34906e01e12c56603c703e537ba90
adavize <adavize@thepoint.africa>
2020-09-11 18:26:35 +0100

    Complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,101 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import github from "./github.svg";
+import "./App.css";
+import ArticleCard from "./articleCard";
+import axios from "axios";
 
 function App() {
+  const [posts, setPosts]: any = useState();
+  const [unsortedPosts, setUnsortedPosts]: any = useState([]);
+
+  useEffect(() => {
+    // let unsortedPosts: any = [];
+    // techpoint
+    axios
+      .get("https://techpoint.africa/wp-json/wp/v2/posts")
+      .then((response) => {
+        response.data.map((postData: any) =>
+          setUnsortedPosts(
+            unsortedPosts.push({
+              ...postData,
+              source: "Techpoint",
+            })
+          )
+        );
+        // setPosts(unsortedPosts);
+        console.log(response.data);
+      });
+
+    // tecgcabal
+    axios.get("https://techcabal.com/wp-json/wp/v2/posts").then((response) => {
+      response.data.map((postData: any) =>
+        setUnsortedPosts(
+          unsortedPosts.push({
+            ...postData,
+            source: "Techcabal",
+          })
+        )
+      );
+      // setPosts(unsortedPosts);
+    });
+    // technext
+    axios.get("https://technext.ng/wp-json/wp/v2/posts").then((response) => {
+      response.data.map((postData: any) =>
+        setUnsortedPosts(
+          unsortedPosts.push({
+            ...postData,
+            source: "Technext",
+          })
+        )
+      );
+    });
+    setPosts(unsortedPosts);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+    <div className="custom-container">
+      <header className="flex justify-between items-center my-10">
+        <div>
+          <span className="text-4xl">👨🏾‍💻</span>
+        </div>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          href="http://github.com/tech-headlines"
+          title="Talk is cheap, show me the code 👏"
         >
-          Learn React
+          <img src={github} alt="github" />
         </a>
       </header>
+
+      <h1 className="text-xl md:text-2xl font-bold w-full md:w-2/3 lg:w-1/2 my-16 text-gray-800">
+        Latest Technology News headlines sourced from{" "}
+        <a className="text-blue-600" href="http://techpoint.africa">
+          Techpoint.africa
+        </a>
+        ,{" "}
+        <a className="text-red-600" href="http://techcabal.com">
+          Techcabal.com
+        </a>{" "}
+        &{" "}
+        <a className="text-red-700" href="http://technext.ng">
+          Technext.ng
+        </a>{" "}
+      </h1>
+
+      <div className="lg:flex lg:justify-between lg:flex-wrap">
+        {posts &&
+          posts.map((post: any) => <ArticleCard key={post.id} post={post} />)}
+      </div>
+
+      <footer className="text-center my-10 text-sm">
+        Built with 💙 by{" "}
+        <a
+          className="underline"
+          href="http://twitter.com/ozorku"
+          title="I no too sabi 👨🏾‍🦱"
+        >
+          Ozorku
+        </a>
+      </footer>
     </div>
   );
 }
---
+++ src/articleCard.tsx
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from "react";
+import moment from "moment";
+import axios from "axios";
+
+const ArticleCard = (props: any) => {
+  const [thumbnail, setThumbnail] = useState();
+  const techpointUrl = "https://techpoint.africa";
+  const techcabalUrl = "https://techcabal.com";
+  const technextUrl = "https://technext.ng";
+
+  useEffect(() => {
+    let baseUrl = "";
+    switch (props.post.source) {
+      case "Techpoint":
+        baseUrl = techpointUrl;
+        break;
+      case "Techcabal":
+        baseUrl = techcabalUrl;
+        break;
+      case "Technext":
+        baseUrl = technextUrl;
+        break;
+      default:
+        break;
+    }
+    axios
+      .get(`${baseUrl}/wp-json/wp/v2/media/${props.post.featured_media}`)
+      .then((response) => {
+        setThumbnail(response.data.source_url);
+        console.log(response.data.source_url);
+      });
+  }, []);
+  return (
+    <div className="border sm:border-none bg-white sm:bg-transparent p-4 sm:p-0 lg:w-1/2 md:flex lg:justify-between mb-10 md:mb-20 rounded-lg">
+      <div className="rounded-lg mb-5 md:mb-0 w-full md:w-2/5">
+        <img
+          src={thumbnail}
+          className="h-64 md:h-40"
+          alt=""
+          style={{
+            borderRadius: "inherit",
+            width: "100%",
+            objectFit: "cover",
+          }}
+        />
+      </div>
+      <div className="w-full md:w-3/5 md:ml-8 md:mr-16 flex flex-col justify-between">
+        <a
+          href={props.post.link}
+          target="_blank"
+          className="md:text-lg font-bold hover:text-orange-400"
+          dangerouslySetInnerHTML={{ __html: props.post.title.rendered }}
+        ></a>
+        <div className="mt-5 md:mt-0">
+          <p>{props.post.source}</p>
+          <p className="text-xs md:text-sm">
+            {moment(props.post.date).format("DD MMMM, YYYY")}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ArticleCard;
